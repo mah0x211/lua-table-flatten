83377fcaaeb87826a54d9e3846ded3b7c72e3f34
Masatoshi Teruya <mah0x211@gmail.com>
2018-05-21 18:29:53 +0900

    first commit

---
+++ flatten.lua
@@ -0,0 +1,126 @@
+--[[
+
+  Copyright (C) 2018 Masatoshi Teruya
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+
+  flatten.lua
+  lua-table-flatten
+  Created by Masatoshi Teruya on 18/05/21.
+
+--]]
+--- file-scope variables
+local type = type
+local next = next
+local tostring = tostring
+--- constants
+local INF_POS = math.huge;
+local INF_NEG = -INF_POS;
+
+
+--- isFinite
+-- @param n
+-- @return ok
+local function isFinite( n )
+    return type( n ) == 'number' and ( n < INF_POS and n > INF_NEG );
+end
+
+
+--- encode
+-- @param key
+-- @param val
+-- @return key
+-- @return val
+local function encode( key, val )
+    -- default do-nothing
+    return key, val
+end
+
+
+--- _flatten
+-- @param tbl
+-- @param maxdepth
+-- @param encoder
+-- @param depth
+-- @param prefix
+-- @param circular
+-- @param res
+-- @return res
+-- @return err
+local function _flatten( tbl, maxdepth, encoder, depth, prefix, res, circular )
+    local k, v = next( tbl )
+
+    while k do
+        if type( v ) ~= 'table' then
+            local key, val = encoder( prefix .. k, v )
+            res[key] = val
+        else
+            local ref = tostring( v )
+
+            -- set value except circular referenced value
+            if not circular[ref] then
+                if maxdepth > 0 and depth >= maxdepth then
+                    res[prefix .. k] = v
+                else
+                    circular[ref] = true
+                    _flatten( v, maxdepth, encoder, depth + 1,
+                              prefix .. k .. '.', res, circular )
+                    circular[ref] = nil
+                end
+            end
+        end
+
+        k, v = next( tbl, k )
+    end
+
+    return res
+end
+
+
+--- flatten
+-- @param tbl
+-- @param maxdepth
+-- @param encoder
+-- @return res
+local function flatten( tbl, maxdepth, encoder )
+    -- veirfy arguments
+    if type( tbl ) ~= 'table' then
+        error( 'tbl must be table' )
+    end
+
+    if maxdepth == nil then
+        maxdepth = 0
+    elseif not isFinite( maxdepth ) then
+        error( 'maxdepth must be finite number' )
+    end
+
+    -- use default encode function
+    if encoder == nil then
+        encoder = encode
+    elseif type( encoder ) ~= 'function' then
+        error( 'encoder must be function' )
+    end
+
+    return _flatten( tbl, maxdepth, encoder, 1, '', {}, {
+        [tostring( tbl )] = true
+    })
+end
+
+
+return flatten
---
+++ rockspecs/table-flatten-scm-1.rockspec
@@ -0,0 +1,21 @@
+package = "table-flatten"
+version = "scm-1"
+source = {
+    url = "git://github.com/mah0x211/lua-table-flatten.git"
+}
+description = {
+    summary = "flatten a table into a table of specified depth.",
+    homepage = "https://github.com/mah0x211/lua-table-flatten",
+    license = "MIT/X11",
+    maintainer = "Masatoshi Teruya"
+}
+dependencies = {
+    "lua >= 5.1",
+}
+build = {
+    type = "builtin",
+    modules = {
+        ["table.flatten"] = "flatten.lua"
+    }
+}
+
